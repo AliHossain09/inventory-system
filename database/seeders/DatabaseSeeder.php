f3091ef15a96207596e6559f77e7d069c0ab7fb4
AliHossain09 <alihoossainn@gmail.com>
2025-12-16 12:06:23 +0600

    setup Product model, factory, migration, and seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,11 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\ProductSeeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,9 +20,8 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            ProductSeeder::class,
         ]);
     }
 }
