a167ae92d3ac098bebc2bfb4bb6e095141c0a129
AliHossain09 <alihoossainn@gmail.com>
2025-12-16 11:09:19 +0600

    install and initialize shadcn-vue UI library

--- resources/js/Pages/Products/Index.vue
+++ resources/js/Pages/Products/Index.vue
@@ -1,11 +1,13 @@
 <template>
   <Head title="Welcome" />
-  <h1 font->Welcome</h1>
+  <h1 class="font-bold">Welcome</h1>
+  <Button>Click Me</Button>
 </template>
 
 
 <script setup>
 import { Head } from '@inertiajs/vue3'
+import { Button } from '@/components/ui/button'
 </script>
 
 <style lang="">
